fbdd05d53ebeabaab6cc2b673ddb10330b617674
skipw20 <skipw20@verizon.net>
2024-07-20 18:32:44 -0500

    Initial

--- AudioClient/AppDelegate.swift
+++ AudioClient/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  AudioClient
+//  MovieClient
 //
-//  Created by Robert Waller on 7/20/24.
+//  Created by Charles Kerr on 2/28/24.
 //
 
 import Cocoa
@@ -11,20 +11,158 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet var window: NSWindow!
+    @IBOutlet var networkOptions: NetworkOptionViewController!
+    @IBOutlet var movieOptions : MovieOptionViewController!
+    @IBOutlet var mediaPlayer : MediaPlayer!
 
+    @IBOutlet var networkHolder : MyView!
+    @IBOutlet var movieHolder : MyView!
+    @IBOutlet var connection: Connection!
+    
+    @objc dynamic var mode = "Start"
+    
+    @objc dynamic var currentState: String = "Stopped"
+    
+    var observation: NSKeyValueObservation?
+    
+    var stateOnTimer:Timer? = nil
+    var stateOffTimer:Timer? = nil
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+    
+    // ============================================================================================
+    deinit {
+        observation?.invalidate()
+        observation = nil
     }
 
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true ;
+    }
+    
+    @objc func stateOnFired(timer:Timer) {
+        DispatchQueue.main.async {
+            if self.networkOptions.connectTime.inRange(), !self.connection.connected {
+                
+                _ = self.connection.connect(serverIP: self.networkOptions.serverAddress, serverPort: String(self.networkOptions.serverPort))
+            }
+        }
+
+    }
+    // ===========================================================================================
+    func scheduleStateOnTimer() {
+        stateOnTimer = Timer(timeInterval: 30.0, target: self, selector: #selector(stateOnFired), userInfo: nil, repeats: true)
+        RunLoop.current.add(stateOnTimer!, forMode: .common)
+        
+    }
+    
+    @objc func stateOffFired(timer:Timer) {
+        DispatchQueue.main.async {
+            if !self.networkOptions.connectTime.inRange(), self.connection.connected {
+                
+                self.connection.stop(error:nil)
+            }
+        }
+    }
+
+    // ===========================================================================================
+    func scheduleStateOffTimer() {
+        stateOffTimer = Timer(timeInterval: 30.0, target: self, selector: #selector(stateOffFired), userInfo: nil, repeats: true)
+        RunLoop.current.add(stateOffTimer!, forMode: .common)
+    }
+    // ============================================================================================
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        networkOptions.loadFromPreference()
+        networkOptions.view.frame = self.networkHolder.bounds
+        self.networkHolder.addSubview(networkOptions.view)
+        
+        movieOptions.loadFromPreference()
+        movieOptions.view.frame = self.movieHolder.bounds
+        self.movieHolder.addSubview(movieOptions.view)
+        
+        
+        observation = observe(\.connection?.connected, options: .new, changeHandler: { [self] object, change in
+            //Swift.print("Network state changed!")
+            if (!self.connection.connected) {
+                mediaPlayer.clear()
+            }
+            self.updateCurrentState() 
+         })
+            
+
+    }
+    // ============================================================================================
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    // ============================================================================================
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
 
+    // ============================================================================================
+   func updateCurrentState() {
+       DispatchQueue.main.async{
+           if (self.connection.connected) {
+               self.currentState = "Connected"
+           }
+           else {
+               if self.mode == "Start" {
+                   self.currentState = "Stopped"
+               }
+               else {
+                   // We are either waiting to connect, or connecting
+                   if (self.networkOptions.connectTime.inRange()) {
+                       self.currentState = "Connecting"
+                   }
+                   else {
+                       self.currentState = "Waiting for connect time: " + self.networkOptions.connectTime.getNextStart()
+                   }
+               }
+           }
+       }
+    }
 
+
+    // ============================================================================================
+    @IBAction func changeAction(_ sender:NSButton) {
+        // First, determine if we can even move on
+        if self.window.makeFirstResponder(sender) {
+            // Now, we determine what mode we are transistioning into?
+            if (mode == "Start") {
+                // We are going to start everything!
+                // Save to our Preferences
+                networkOptions.saveToPreference()
+                movieOptions.saveToPreference()
+                connection.myHandle = networkOptions.handle
+                networkOptions.isEnabled = false
+                movieOptions.isEnabled = false
+                
+                mode = "Stop"
+                scheduleStateOffTimer()
+                scheduleStateOnTimer()
+                if networkOptions.connectTime.inRange() {
+                    // We should connect
+                    _ = connection.connect(serverIP: networkOptions.serverAddress, serverPort: String(networkOptions.serverPort))
+                }
+            }
+            
+            else {
+                // We want to stop any network stuff and timers
+                self.mediaPlayer.clear()
+                networkOptions.isEnabled = true
+                movieOptions.isEnabled = true
+                connection.stop(error: nil)
+                stateOnTimer?.invalidate()
+                stateOnTimer = nil
+                stateOffTimer?.invalidate()
+                stateOffTimer = nil
+                mode = "Start"
+            }
+            updateCurrentState()
+
+        }
+    }
 }
 
---
+++ AudioClient/Player/MediaWindowController.swift
@@ -0,0 +1,131 @@
+//Copyright © 2024 Chales Kerr. All rights reserved.
+
+import Cocoa
+
+class MediaWindowController : NSWindowController {
+    
+    @IBOutlet weak var mediaPlayer:MediaPlayer!
+    @IBOutlet weak var movieOptions:MovieOptionViewController!
+    
+
+    var isObserving:Bool = false
+    
+    override var windowNibName: NSNib.Name? {
+        return "MediaWindow"
+    }
+    
+    // =======================================================================================================================
+    func setShow(state:Bool) {
+        if (state) {
+            //Swift.print("Bring up Show Window")
+            self.showWindow(nil)
+            if (!self.window!.isFullscreen) {
+                //self.window?.toggleFullScreen(nil)
+            }
+        }
+        else {
+            
+            if (NSScreen.screens.count < 2) {
+                self.window?.close()
+            }
+            mediaPlayer.clear()
+            
+        }
+    }
+    // =====================================================================================
+    func setupNotificationCenter() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleDisplayConnection),
+            name: NSApplication.didChangeScreenParametersNotification,
+            object: nil)
+        isObserving = true
+    }
+
+
+    
+    // =======================================================================================================================
+    deinit {
+        if (isObserving) {
+            NotificationCenter.default.removeObserver(self)
+        }
+    }
+    
+    // ======================================================================================================================
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        
+        mediaPlayer.playerLayer.frame = self.window!.contentView!.layer!.bounds
+        self.window!.contentView!.layer?.addSublayer(mediaPlayer.playerLayer)
+    }
+
+    // ======================================================================================================================
+    @objc func handleDisplayConnection(notification: Notification) {
+        
+        if (NSScreen.screens.count > 1) {
+            // We have more then one screen, we should be on the second screen
+            if (self.window!.screen == NSScreen.main) {
+                // We need to move this window to the second window
+                // It is!, we need to move it
+                let size = NSScreen.screens[1].visibleFrame.size
+                let origin = NSScreen.screens[1].visibleFrame.origin
+                self.window!.setFrame(NSRect(origin: origin, size: size), display: self.window!.isVisible )
+                if (!self.window!.isFullscreen) {
+                    self.window?.toggleFullScreen(nil)
+                }
+                
+            }
+           
+        }
+        else if ( NSScreen.screens.count < 2) {
+            if (self.window!.screen != NSScreen.main) {
+                // We need to move this window to the main window
+                let size = NSScreen.screens[0].visibleFrame.size
+                let origin = NSScreen.screens[0].visibleFrame.origin
+                self.window!.setFrame(NSRect(origin: origin, size: size), display: self.window!.isVisible )
+                if (self.window!.isFullscreen) {
+                    self.window?.toggleFullScreen(nil)
+                }
+
+            }
+
+        }
+    }
+
+    // =======================================================================================================================
+    func loadMovie(moviename:String) {
+
+        let completename = moviename + ".wav"
+        guard let url = movieOptions.movieLocation?.appendingPathComponent(completename) else {
+            return
+        }
+        //Swift.print("Load movie: \(url.path())")
+
+        mediaPlayer.loadMovie(url: url)
+    }
+    
+    // =======================================================================================================================
+    func sync(frame:Int32) {
+        let delta = mediaPlayer.currentFrame - frame
+        if  abs(delta) > 6 {
+            //Swift.print("Sync movie to frame: \(frame)")
+
+            mediaPlayer.setFrame(frame: frame)
+        }
+    }
+    
+    // =======================================================================================================================
+    func playMovie(state:Bool,frame:Int32 = 0) {
+        if (state) {
+            //Swift.print("Play movie at frame: \(frame)")
+            mediaPlayer.setFrame(frame: frame)
+            mediaPlayer.play()
+        }
+        else {
+            //Swift.print("Stop movie ")
+            mediaPlayer.stop()
+        }
+    }
+    
+
+}
